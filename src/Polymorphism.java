6f8cccb0f6e4c3f687f1986af3ce357957dfc926
kimso34 <skylar.kim@mail.utoronto.ca>
2026-05-15 01:26:44 -0400

    made more adjustments to Polymorphism.java

--- src/Polymorphism.java
+++ src/Polymorphism.java
@@ -6,31 +6,42 @@ public class Polymorphism {
      * @param p the person whose role is to be returned
      * @return the role of the person
      */
-    public static String getStudentRole(Person p) {
-        // TODO: right now for student, it is not returning the role to be student.
+    public static String getRole(Person p) {
+
+        if (p instanceof Student) {
+
+            return ((Student) p).role;
+        }
+
         return p.role;
     }
 }
 
 class Person {
-    public final String role = "General";
-    private final String name;
+    public String role = "General";
+    private String name;
 
     public Person(String name) {
         this.name = name;
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
 }
 
 class Student extends Person {
+
     public final String role = "Student";
     private final String studentId;
 
     public Student(String name, String studentId) {
+
         super(name);
         this.studentId = studentId;
+    }
+
+    public String getStudentId() {
+        return studentId;
     }
 }
